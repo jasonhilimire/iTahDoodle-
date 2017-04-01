d1d160513c5eabdc69ae0611ab3915996e06ace6
jasonhilimire <jasonhilimire@mac.com>
2017-04-01 15:49:17 -0600

    updated keyboard done press adds item to todo list

--- iTahDoodle/ViewController.swift
+++ iTahDoodle/ViewController.swift
@@ -25,39 +25,48 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+// When insert button is pressed add textFieldEntry
     @IBAction func addButtonPressed(_ sender: UIButton) {
-        if itemTextField.text == "" {
-        } else {
-        
-        guard let todo = itemTextField.text else {
-            return
-        }
-        todoList.add(todo)
-        tableView.reloadData()
-        itemTextField.text = ""
-        }
+       textFieldEntry()
+    }
+    
+// When done key on keyboard is pressed add textFieldEntry
+    @IBAction func editingDidEnd(_ sender: UITextField) {
+        textFieldEntry()
     }
     
 
-    // dismisses keyboard when you tap outside the textbox
+// dismisses keyboard when you tap outside the textbox
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         self.view.endEditing(true)
     }
     
-    // dismisses keyboard when you press return on the keyboard
+// dismisses keyboard when you press return on the keyboard
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
-        // shut down keyboard associated with textfield being edited
-        
+    // shut down keyboard associated with textfield being edited
         textField.resignFirstResponder()
         
         return true
     }
+
+    
+// itemTextField text is created as a todo
+    func textFieldEntry(){
+        if itemTextField.text == "" {
+        } else {
+            
+            guard let todo = itemTextField.text else {
+                return
+            }
+            todoList.add(todo)
+            tableView.reloadData()
+            itemTextField.text = ""
+        }
+    }
+    
+    
 }
 
